40ecf28022629a1a835dd100cf62af5788a87c9a
fuka-innovations <fuad.kalhori@gmail.com>
2022-01-16 11:10:01 +0200

    removed header padding for android

--- UX/Week-1/utils/GlobalStyles.js
+++ UX/Week-1/utils/GlobalStyles.js
@@ -4,6 +4,6 @@ export default StyleSheet.create({
   AndroidSafeArea: {
     flex: 1,
     backgroundColor: 'black',
-    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    //paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
   },
 });
